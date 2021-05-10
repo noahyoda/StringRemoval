49fcf3ba16284a08e1bef9b23293e6f73d1d07fe
noahyoda <67946197+noahyoda@users.noreply.github.com>
2021-05-10 13:59:27 -0700

    Add files via upload

--- Removal.java
+++ Removal.java
@@ -13,19 +13,28 @@ import java.io.OutputStreamWriter;
  */
 public class Removal {
 
-	public static void antimatter(String line) {
+	/**
+	 * main user method to pass string and remove in fastest method
+	 * 
+	 * @param string of line
+	 * @param string to remove
+	 */
+	public static void antimatter(String line, String remove) {
 		StringBuilder words = new StringBuilder();
 		words.append(line);
-		printRemove(words);
+		printRemove(words, remove);
 	}
-	
-	private static void printRemove(StringBuilder phrase) {
+	/**
+	 * pass the string builder 
+	 * @param phrase
+	 */
+	private static void printRemove(StringBuilder phrase, String remove) {
 		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-		int indexA = phrase.indexOf("antimatter");
+		int indexA = phrase.indexOf(remove);
 		while(indexA >= 0) {
-			phrase.replace(indexA, indexA + 9, "");
+			phrase.replace(indexA, indexA + remove.length(), "");
 			int temp = indexA;
-			indexA = phrase.indexOf("antimatter", temp);
+			indexA = phrase.indexOf(remove);
 		}
 		try {
 			out.write(phrase.toString());
